b9254d8bc6690f94daff3e5f272f9957dbc5548e
Amsrtss <p4tchouli.knowledge@gmail.com>
2024-12-17 09:08:23 +0700

    Fitur Login

--- Program.cs
+++ Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Login()
+    {
+        try
+        {
+            Console.WriteLine("Selamat Datang di Penyewaan Lapangan Futsal");
+            Console.WriteLine("SIlahkan Login!");
+            Console.Write("Username: ");
+            string username = Console.ReadLine();
+
+            Console.Write("Password: ");
+            string password = Console.ReadLine();
+
+            if (username == "admin" && password == "admin")
+            {
+                Console.WriteLine("Login Berhasil!");
+                // menuAdmin();
+            }
+            else if (username == "penyewa" && password == "p")
+            {
+                Console.WriteLine("Login Berhasil!");
+                // menuPenyewa();
+            }
+            else
+            {
+                Console.WriteLine("Username atau Password salah, silahkan coba lagi!");
+                Login();
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Terjadi Kesalahan: {e.Message}");
+            Login();
+        }
+
+    }
+    public static void Main(string[] args)
+    {
+    }
+}
